c04b73aa37362312c2fe56841c1b126749500982
nickola102 <nickola@list.ru>
2022-01-12 11:10:12 +0300

    new_cost

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int cost = 5000; // стоимость авиабилета
+        int cost = 5_575; // стоимость авиабилета
         int mile = 20; // количество рублей для одной бонусной мили
 
         int bonus = cost / mile; // количество бонусных миль
