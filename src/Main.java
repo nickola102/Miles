b59a2132ded99554809fdbeee7817cf5c0b2636e
nickola102 <nickola@list.ru>
2022-01-12 10:53:27 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 5000; // стоимость авиабилета
+        int mile = 20; // количество рублей для одной бонусной мили
+
+        int bonus = cost / mile; // количество бонусных миль
+
+        System.out.println((bonus) + "  бонусных миль");
+
+    }
+
+}
