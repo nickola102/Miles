598d511e4f51a3290e138f0552855c9d25ad1e45
nickola102 <nickola@list.ru>
2022-01-18 12:35:26 +0300

    BonusMiles

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int cost = 5_575; // стоимость авиабилета
-        int mile = 20; // количество рублей для одной бонусной мили
-
-        int bonus = cost / mile; // количество бонусных миль
-
-        System.out.println((bonus) + "  бонусных миль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
 
     }
 
